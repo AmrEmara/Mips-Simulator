f4f0d6b60aa67d8c830c6aee88213e80fd466873
karimFarid <karim.f.1995@gmail.com>
2015-04-22 18:29:25 +0200

    Update Simulator.java
    changing my method to void,creating a hashtable containing controlSignals, calling execute.

--- src/Simulator.java
+++ src/Simulator.java
@@ -11,46 +11,96 @@ public class Simulator {
 		registerFile = new HashMap<String, String>();
 	}
 
-	public static Hashtable<String,String>  decoder(String binary) {
-		Hashtable<String,String> toExecute = null;
-		toExecute.put("Operation Code", binary.substring(0, 5));
+	public static void decoder(String binary) {
+		String rs, rt, rd;
+		Hashtable<String, String> toExecute = null;
+		Hashtable<String, String> controlSignals=null;
+		toExecute.put("op", binary.substring(0, 5));
 		if (binary.startsWith("000000"))
-			//if R-format, sends back all the data .
+		// if R-format, sends back all the data .
 		{
-			// format ="R";
-			//shft = binary.substring(21,25);
-			// funct = binary.substring(26);
-			
-			rs = binary.substring(6,10);
-			rt = binary.substring(11,15);
-			rs= registerFile.get(rs); 
-			rt= registerFile.get(rt);	
-			toExecute.put("First Source", rs);
-			toExecute.put("Destination Register", binary.substring(16,20));
-			toExecute.put("Second Source", rt);
-			toExecute.put("Shift Amount", binary.substring(21,25));
-			toExecute.put("Function", binary.substring(26));	
-		}
-		else if (binary.startsWith("000010") || binary.startsWith("000011"))
+			//set control signals.
+			controlSignals.put("RegDst","1");
+			controlSignals.put("ALUSrc","0");
+			controlSignals.put("MemtoReg","0");
+			controlSignals.put("RegWrite","1");
+			controlSignals.put("MemRead","0");
+			controlSignals.put("MemWrite","0");
+			controlSignals.put("Branch","0");
+			controlSignals.put("ALUOp","10");
+			// read the values in rs and rt and place them in registers
+			rs = binary.substring(6, 10);
+			rt = binary.substring(16, 20);
+			rs = registerFile.get(rs);
+			rt = registerFile.get(rt);
+			toExecute.put("firstSource", rs);
+			toExecute.put("destinationRegister", binary.substring(11, 15));
+			toExecute.put("secondSource", rt);
+			toExecute.put("shamt", binary.substring(21, 25));
+			toExecute.put("funct", binary.substring(26));
+		} else if (binary.startsWith("000010") || binary.startsWith("000011"))
 		// if J-format ,sends back the address.
 		{
-			toExecute.put("Address",binary.substring(6));
-		}
-		else 
+
+			toExecute.put("address", binary.substring(6));
+		} else
 		// if I-format sends back the value of reg,destination ,constant/adress.
 		{
-			//format ="I";
-			//rt = binary.substring(11,15);
-			//imm= binary.substring(16);
-			
-			
-			rs = binary.substring(6,10);
-			rs= registerFile.get(rs); 
-			toExecute.put("Source Register", rs);
-			toExecute.put("Destination Register", binary.substring(11,15));
-			toExecute.put("Constant-Address", binary.substring(16));
+			// format ="I";
+			// rt = binary.substring(11,15);
+			// imm= binary.substring(16);
+
+			rs = binary.substring(6, 10);
+			rs = registerFile.get(rs);
+			toExecute.put("sourceRegister", rs);
+			toExecute.put("destinationRegister", binary.substring(11, 15));
+			toExecute.put("address", binary.substring(16));
+			if (binary.startsWith("001000")){ 
+				// if it's a addi ,set control signals.
+				controlSignals.put("RegDst","0");
+				controlSignals.put("ALUSrc","1");
+				controlSignals.put("MemtoReg","0");
+				controlSignals.put("RegWrite","1");
+				controlSignals.put("MemRead","0");
+				controlSignals.put("MemWrite","0");
+				controlSignals.put("Branch","0");
+				controlSignals.put("ALUOp","00");
+			}
+			else if (binary.startsWith("100011")){
+				// if it's load word,set control signals.
+				controlSignals.put("RegDst","0");
+				controlSignals.put("ALUSrc","1");
+				controlSignals.put("MemtoReg","1");
+				controlSignals.put("RegWrite","1");
+				controlSignals.put("MemRead","1");
+				controlSignals.put("MemWrite","0");
+				controlSignals.put("Branch","0");
+				controlSignals.put("ALUOp","00");
+			}
+			else if (binary.startsWith("100011")){
+				// if it's save word,set control signals.
+				controlSignals.put("RegDst","X");
+				controlSignals.put("ALUSrc","1");
+				controlSignals.put("MemtoReg","X");
+				controlSignals.put("RegWrite","0");
+				controlSignals.put("MemRead","0");
+				controlSignals.put("MemWrite","1");
+				controlSignals.put("Branch","0");
+				controlSignals.put("ALUOp","00");
+			}
+			else if (binary.startsWith("100011")){
+				// if it's Branch on equal,set control signals.
+				controlSignals.put("RegDst","X");
+				controlSignals.put("ALUSrc","0");
+				controlSignals.put("MemtoReg","X");
+				controlSignals.put("RegWrite","0");
+				controlSignals.put("MemRead","0");
+				controlSignals.put("MemWrite","0");
+				controlSignals.put("Branch","1");
+				controlSignals.put("ALUOp","00");
+			}
 		}
-		return toExecute;
-			
+		Execute(toExecute,controlSignals);
+
 	}
 }
